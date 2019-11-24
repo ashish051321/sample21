51743491ffbb716941c0c2d02f7fbbeb611d2285
ashish051321 <ashish.25jl@gmail.com>
2019-11-25 01:58:00 +0530

    1 file modified

--- common/constants.ts
+++ common/constants.ts
@@ -7,6 +7,6 @@ export const loginCardImages = [
   {
     title:'Work with the Best in the Industry',
     desc:'Accelerate your career with B2B Marketting Leaders.',
-    imageUrl : 'https://www.xdbscorp.com/wp-content/uploads/2019/06/IMG_1018-1.jpg'
+    imageUrl : 'https://source.unsplash.com/1024x768/?team'
   }
 ]
