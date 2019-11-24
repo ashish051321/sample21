2e44fc9e7afc7a4df0bb9520858ccd7ce2286b3b
ashish051321 <ashish.25jl@gmail.com>
2019-11-25 01:51:10 +0530

    2 files modified, 6 added, 7 deleted

--- pages/login/login.ts
+++ pages/login/login.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import  {ScreenConstants, loginCardImages} from '../../common/constants';
-
+import { Component, OnInit } from "@angular/core";
+import { ScreenConstants, loginCardImages } from "../../common/constants";
+import { interval } from "rxjs";
+import { delay, first, mapTo } from "rxjs/operators";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.html',
-  styleUrls: ['./login.css']
+  selector: "app-login",
+  templateUrl: "./login.html",
+  styleUrls: ["./login.css"]
 })
 export class LoginPage implements OnInit {
+  showSplash = true;
   screenConst = ScreenConstants;
   loginCardImages = loginCardImages;
-  constructor() {
-    
-   }
+  constructor() {}
 
   ngOnInit() {
+    interval(1000)
+      .pipe(
+        delay(5000),
+        first(),
+        mapTo(false)
+      )
+      .subscribe(data => {
+        this.showSplash = data; 
+      });
   }
-
 }
